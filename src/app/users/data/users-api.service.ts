a2426ecc6406e45dd111362e6c2339a55758b036
Alfredo Perez <alfredo.perez.q@gmail.com>
2024-11-07 06:27:00 -0500

    fix: correct import path in users-api service
    - Adjusted ApiService import path to use relative path

--- src/app/users/data/users-api.service.ts
+++ src/app/users/data/users-api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ApiService } from '@my/shared/data/api.service';
+import { ApiService } from '../../shared/data/api.service';
 import { User } from './users.models';
 
 @Injectable({ providedIn: 'root' })
